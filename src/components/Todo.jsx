1c03390001cd49bd947c18d17abe1db4d04f15a4
Utsav Bhattarai <ubhattarai05@gmail.com>
2022-09-01 11:29:18 +0545

    udpated table

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,20 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Todo.css'
 
-export const Todo = () => {
+const Todo = () => {
+  const[inputData,setInputData]=useState('')
+  const[todos, setTodos]=useState([
+    'Walk for fresh',
+    'Rest a while.',
+    'feed a dog.',
+    'Watch a movie.'
+  ])
+  console.log(inputData);
+
+  // arrow function can also be used 
+  // const handleSubmit= =>{}
+
+
+  // const handelSubmit=()=>{}
+  function handleSubmit(){
+    setTodos([...todos,inputData]);
+    setInputData('');
+
+  }
+  function removeall(){
+    setTodos([]);
+    
+
+  }
+
+
+
   return (
     <div class="container">
         <h3 class="app-logo">Todo App</h3><br></br>
-        <input type="text" class="enter" placeholder='Enter' ></input>
-        <button class ="add">Add Todo</button>
+
+
+        <input
+         type="text" 
+         class="enter"
+         placeholder='Enter'
+         value={inputData}
+         onChange={(e) => setInputData(e.target.value)}
+         
+         ></input>
+
+
+
+        <button class ="add"
+        onClick={handleSubmit}
+        disabled={!inputData}
+        >Add Todo</button>
+
+
         <h3 class="dot"><u>Todo List</u></h3>
+
+
         <ul class="list">
-            <li >Hello </li>
-            <li>Task 1</li>
-            <li>Task 2</li>
+
+          
+            {todos.map ((todo) => (
+              <li>{todo}  <button>edit</button><button>delete</button></li>
+              
+            ))}
+
         </ul>
-        <button class="remove">Remove A/C</button>
+        <button class="remove"
+
+        onClick={removeall}
+        >Remove A/C</button>
     </div>
+
   )
-}
+            }
  export default Todo
+            
